4bc9a30c69a6d0c337ae54b76592bea97c315f8e
Dagadev <gacub20ru@gmail.com>
2023-07-13 07:16:41 -0600

    update config

--- dist/config.js
+++ dist/config.js
@@ -8,12 +8,12 @@ exports.ENVIRONMENTS = {
     CONNECT_MONGO: process.env.MONGO_CONECT,
     SECRET: process.env.SECRET,
     DISCORD: process.env.DISCORD,
-    QUTOOL_ID: process.env.QUTOOL_ID,
+    QUTOOL_ID: process.env.QUTOOL_ID || '935707268090056734',
     QUTOOL_SECRET: process.env.QUTOOL_SECRET,
     DOMAIN: process.env.DOMAIN || 'http://localhost:246',
     SESSION_SECRET: process.env.DOMAIN || 'HelloQutool',
     BOT_TOKEN: process.env.BOT_TOKEN,
-    PAGE_DOMAIN: process.env.PAGE_DOMAIN
+    PAGE_DOMAIN: process.env.PAGE_DOMAIN || 'http://localhost:3000'
 };
 exports.APP_ID = '09012004';
 exports.PATH_PREFIX = '/api/v1/';
