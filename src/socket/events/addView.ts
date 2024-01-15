c7361dac9c64f81fd1e1c0a77c81cd80a338e4aa
Dagadev <gacub20ru@gmail.com>
2024-01-15 12:56:41 -0600

    add addView event

--- src/socket/events/addView.ts
+++ src/socket/events/addView.ts
@@ -1,12 +1,35 @@
-import { type Socket } from 'socket.io'
-import { APP_ID } from '../../utils/config'
 import { WebAnalytics } from '../../models'
+import type { SocketData, AnalyticsEventsProps } from '../../types'
 
-export const addViewEvent = async (socket: Socket) => {
-  const portfolio = await WebAnalytics.findById(APP_ID)
-  if (typeof portfolio?.views === 'number') {
-    portfolio.views++
-    socket.broadcast.emit('view', portfolio.views)
-    await portfolio.save()
-  } else socket.broadcast.emit('view', 0)
+export async function addViewEvent (socket: SocketData, { id, browserID }: AnalyticsEventsProps) {
+  const origin = socket.handshake.headers.origin ?? 'none'
+
+  const updateConfig = [
+    { $inc: { view: 1 } },
+    { new: true }
+  ]
+  const byOrigin = id === undefined
+  const schemaMethod = byOrigin ? WebAnalytics.findOneAndUpdate({ origin }, ...updateConfig) : WebAnalytics.findByIdAndUpdate(id, ...updateConfig)
+  const Analytics = await schemaMethod
+
+  if (Analytics === null) {
+    socket.emit('analytics', undefined)
+    return
+  }
+
+  const browser = Analytics.browsers.find(b => b.id === browserID)
+
+  if (browser === undefined) {
+    Analytics.browsers.push({
+      id: browserID,
+      lastVisitAt: Date.now()
+    })
+    Analytics.save()
+    return
+  }
+
+  browser.lastVisitAt = Date.now()
+
+  socket.emit('analytics', Analytics)
+  Analytics.save()
 }
