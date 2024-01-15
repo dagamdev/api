5b167e996de9c06f70a403a194d6d76641eac181
Dagadev <gacub20ru@gmail.com>
2024-01-14 23:01:08 -0600

    update socket types

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,16 +2,11 @@ import { Server as SocketServer } from 'socket.io'
 import http from 'http'
 import { app } from '../app'
 import { connnectionEvent } from './events/connection'
-import type { ClientToServerEvents, InterServerEvents, ServerToClientEvents, SocketData } from '../types'
+import type { ServerToClientEvents, ClientToServerEvents } from '../types'
 import { ORIGINS } from '../utils/config'
 
 export const server = http.createServer(app)
-export const io = new SocketServer<
-ClientToServerEvents,
-ServerToClientEvents,
-InterServerEvents,
-SocketData
->(server, {
+export const io = new SocketServer<ServerToClientEvents, ClientToServerEvents>(server, {
   cors: {
     origin: ORIGINS,
     credentials: true
