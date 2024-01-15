293255d9ea1fdaaac5e912c67ef89afb9693890c
Dagadev <gacub20ru@gmail.com>
2024-01-14 22:51:29 -0600

    add browser schema

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,14 @@
 import { Schema, model } from 'mongoose'
 
+const Browser = new Schema({
+  id: { type: String, require: true, default: '' },
+  liked: { type: Boolean, require: true, default: false },
+  lastVisitAt: { type: Number, require: true, default: 0 }
+})
+
 export const WebAnalytics = model('web analytics', new Schema({
   views: { type: Number, default: 1 },
   likes: { type: Number, default: 0 },
   origin: { type: String, require: true },
-  browserIDs: { type: [String], require: true }
+  browsers: { type: [Browser], require: true }
 }))
