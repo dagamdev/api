3316fa5f97e2665d8d64d8ed90eab842a5d21db8
Dagadev <gacub20ru@gmail.com>
2024-01-15 12:57:43 -0600

    add addLike event

--- src/socket/events/addLike.ts
+++ src/socket/events/addLike.ts
@@ -1,12 +1,36 @@
-import { type Socket } from 'socket.io'
-import { APP_ID } from '../../utils/config'
 import { WebAnalytics } from '../../models'
+import type { SocketData, AnalyticsEventsProps } from '../../types'
 
-export const addLikesEvent = async (socket: Socket) => {
-  const portfolio = await WebAnalytics.findById(APP_ID)
-  if (typeof portfolio?.likes === 'number') {
-    portfolio.likes++
-    socket.broadcast.emit('like', portfolio.likes)
-    await portfolio.save()
-  } else socket.broadcast.emit('like', 0)
+export async function addLikeEvent (socket: SocketData, { id, browserID }: AnalyticsEventsProps) {
+  const origin = socket.handshake.headers.origin ?? 'none'
+
+  const updateConfig = [
+    { $inc: { likes: 1 } },
+    { new: true }
+  ]
+  const byOrigin = id === undefined
+  const schemaMethod = byOrigin ? WebAnalytics.findOneAndUpdate({ origin }, ...updateConfig) : WebAnalytics.findByIdAndUpdate(id, ...updateConfig)
+  const Analytics = await schemaMethod
+
+  if (Analytics === null) {
+    socket.emit('analytics', undefined)
+    return
+  }
+
+  const browser = Analytics.browsers.find(b => b.id === browserID)
+
+  if (browser === undefined) {
+    Analytics.browsers.push({
+      id: browserID,
+      liked: true,
+      lastVisitAt: Date.now()
+    })
+    Analytics.save()
+    return
+  }
+
+  browser.liked = true
+
+  socket.emit('analytics', Analytics)
+  Analytics.save()
 }
