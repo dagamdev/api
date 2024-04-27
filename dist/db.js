214fc52085c1561f3aff7a20d9af0e7ec22c9ef1
Dagadev <gacub20ru@gmail.com>
2024-04-27 12:13:24 -0600

    Remove myMember variable

--- dist/db.js
+++ dist/db.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.myMember = void 0;
 const mongoose_1 = require("mongoose");
 const config_1 = require("./utils/config");
 (0, mongoose_1.set)('strictQuery', true);
@@ -8,4 +7,3 @@ if (config_1.ENVIRONMENTS.CONNECT_MONGO !== undefined) {
     (0, mongoose_1.connect)(config_1.ENVIRONMENTS.CONNECT_MONGO).then(() => { console.log('✅ Connected to database'); })
         .catch(err => { console.log(err); });
 }
-exports.myMember = undefined;
--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,8 @@
 import { connect, set } from 'mongoose'
 import { ENVIRONMENTS } from './utils/config'
-import { type GuildMember } from 'discord.js'
 
 set('strictQuery', true)
 if (ENVIRONMENTS.CONNECT_MONGO !== undefined) {
   connect(ENVIRONMENTS.CONNECT_MONGO).then(() => { console.log('✅ Connected to database') })
     .catch(err => { console.log(err) })
 }
-
-export const myMember: GuildMember | undefined = undefined
