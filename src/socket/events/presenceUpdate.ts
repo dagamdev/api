7a9b0321158871c7b00b49bebec27df3c5f2f58e
Dagadev <gacub20ru@gmail.com>
2024-04-27 12:15:55 -0600

    Remove presence update event

--- src/socket/events/presenceUpdate.ts
+++
@@ -1,5 +0,0 @@
-import type { SocketData, CustomPresence } from '../../types'
-
-export async function presenceUpdateEvent (socket: SocketData, presence: CustomPresence) {
-  socket.emit('presenceUpdate', presence)
-}
