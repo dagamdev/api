6e9ccb6f5a55d97427ed90cfc8d7ae20776f66b7
Dagadev <gacub20ru@gmail.com>
2024-04-27 12:18:12 -0600

    Update connection socket event

--- dist/socket/events/connection.js
+++ dist/socket/events/connection.js
@@ -4,11 +4,7 @@ exports.connnectionEvent = void 0;
 const addView_1 = require("./addView");
 const addLike_1 = require("./addLike");
 const removeLike_1 = require("./removeLike");
-const client_1 = require("../../client");
-const presenceUpdate_1 = require("./presenceUpdate");
-const consts_1 = require("../../utils/consts");
 const connnectionEvent = (socket) => {
-    // console.log({ id: socket.id })
     socket.on('addView', (data) => {
         (0, addView_1.addViewEvent)(socket, data);
     });
@@ -17,21 +13,6 @@ const connnectionEvent = (socket) => {
     });
     socket.on('removeLike', (data) => {
         (0, removeLike_1.removeLikeEvent)(socket, data);
-    });
-    client_1.MyBot.on('presenceUpdate', (oldPresence, newPresence) => {
-        var _a;
-        if (typeof (oldPresence === null || oldPresence === void 0 ? void 0 : oldPresence.guild) === 'object' && ((_a = oldPresence.guild) === null || _a === void 0 ? void 0 : _a.id) !== consts_1.DISCORD.GUILD_ID)
-            return;
-        if ((oldPresence === null || oldPresence === void 0 ? void 0 : oldPresence.userId) !== consts_1.DISCORD.USER_ID)
-            return;
-        // console.log({msg: 'Update user', newPresence})
-        const activities = newPresence === null || newPresence === void 0 ? void 0 : newPresence.activities.map((m) => { var _a; return (Object.assign(Object.assign({}, m), { emoji: (_a = m.emoji) === null || _a === void 0 ? void 0 : _a.name })); });
-        const updatePresence = {
-            status: newPresence.status,
-            activities,
-            clientStatus: newPresence.clientStatus
-        };
-        (0, presenceUpdate_1.presenceUpdateEvent)(socket, updatePresence);
     });
 };
 exports.connnectionEvent = connnectionEvent;
--- src/socket/events/connection.ts
+++ src/socket/events/connection.ts
@@ -1,15 +1,9 @@
 import { addViewEvent } from './addView'
 import { addLikeEvent } from './addLike'
 import { removeLikeEvent } from './removeLike'
-import { MyBot as client } from '../../client'
-import { presenceUpdateEvent } from './presenceUpdate'
-import type { CustomPresence, SocketData } from '../../types'
-import { DISCORD } from '../../utils/consts'
-import { type Activity } from 'discord.js'
+import type { SocketData } from '../../types'
 
 export const connnectionEvent = (socket: SocketData) => {
-  // console.log({ id: socket.id })
-
   socket.on('addView', (data) => {
     addViewEvent(socket, data)
   })
@@ -20,21 +14,5 @@ export const connnectionEvent = (socket: SocketData) => {
 
   socket.on('removeLike', (data) => {
     removeLikeEvent(socket, data)
-  })
-
-  client.on('presenceUpdate', (oldPresence, newPresence) => {
-    if (typeof oldPresence?.guild === 'object' && oldPresence.guild?.id !== DISCORD.GUILD_ID) return
-    if (oldPresence?.userId !== DISCORD.USER_ID) return
-
-    // console.log({msg: 'Update user', newPresence})
-
-    const activities = newPresence?.activities.map((m: Activity) => ({ ...m, emoji: m.emoji?.name })) as Activity[]
-    const updatePresence: CustomPresence = {
-      status: newPresence.status,
-      activities,
-      clientStatus: newPresence.clientStatus
-    }
-
-    presenceUpdateEvent(socket, updatePresence)
   })
 }
