da684fa6bdb835eb44c57629796e8ab9f0500b98
Dagadev <gacub20ru@gmail.com>
2024-04-27 12:12:55 -0600

    Update types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,4 @@
 import { type Socket } from 'socket.io'
-import type { Activity, ClientPresenceStatusData, PresenceStatus } from 'discord.js'
 
 export interface ServerToClientEvents {
   analytics: (analytics: any) => void
@@ -17,34 +16,4 @@ export type SocketData = Socket<ClientToServerEvents, ServerToClientEvents>
 export interface AnalyticsEventsProps {
   id?: string
   browserID: string
-}
-
-export interface IARoom {
-  userId: string
-  channelId?: string
-}
-
-export type Languages = 'en' | 'es' | 'it'
-
-export interface User {
-  _id: string
-  userId: string
-  accessToken: string
-  refreshToken: string
-  __v: number
-}
-
-export interface Guild {
-  id: string
-  name: string
-  icon: string | null
-  owner: boolean
-  permissions: string
-  features: string[]
-}
-
-export interface CustomPresence {
-  status: PresenceStatus
-  activities: Activity[]
-  clientStatus: ClientPresenceStatusData | null
 }
