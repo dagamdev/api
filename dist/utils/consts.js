656b18baab2be24dae96de7218e26c0f9c3e40a6
Dagadev <gacub20ru@gmail.com>
2024-04-27 12:16:32 -0600

    Remove constants

--- dist/utils/consts.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DISCORD = void 0;
-exports.DISCORD = {
-    USER_ID: '717420870267830382',
-    GUILD_ID: '1064289165879025836'
-};
